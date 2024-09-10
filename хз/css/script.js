680ab70c44c362da8d7c6be2ff79829ca3f7565f
BOBA521 <aliskapuper@gmail.com>
2024-09-10 18:01:21 +1000

    Add files via upload

---
+++ хз/css/script.js
@@ -0,0 +1,13 @@
+// let hour = 9;
+// if (hour < 10 || hour > 18) {
+//     alert('Офис закрыт.');
+// }
+
+// 
+const user = {
+    name: "jacob",
+    age: 30
+};
+
+let key = prompt("что вы хотите узнать о пользователе?");
+alert( user[key] );
